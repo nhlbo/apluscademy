511db8a0cca362d2a841a59cae90f7afeac8e4c3
Chánh Quang <47215667+MeanSoybean@users.noreply.github.com>
2022-12-19 17:17:43 +0700

    feat: db modeling course, category, review
    added schema for courses, course categories, and reviews
    see Trello task db modeling iteration 2 for reqs on syllabus content

---
+++ src/models/category.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+import { ICourse } from './course'
+
+interface ICategory {
+    name: string
+    getLastWeekRegistrationCount(): number // used in the query "Which category has had the most registrations in the last week?"
+    getCourses(): Array<ICourse>
+}
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name'],
+    trim: true,
+    index: true
+  },
+  courses: [mongoose.Types.ObjectId]
+})
+
+
+const Category = mongoose.model('Course', categorySchema)
+
+export { ICategory, Category }
---
+++ src/models/course.ts
@@ -0,0 +1,88 @@
+import mongoose from 'mongoose'
+import { ICategory } from './category'
+import { IUser } from './user'
+import { IReview } from './review'
+
+interface ICourse {
+    category: ICategory
+    lecturer: IUser
+    getReviews(): Array<IReview>
+}
+
+
+const courseSchema = new mongoose.Schema({
+    category: {
+        type: mongoose.Types.ObjectId,
+        required: [true, 'Please select a category'],
+    },
+    lecturer: {
+        type: mongoose.Types.ObjectId,
+        required: [true, 'Course must have a lecturer']
+    },
+    title: {
+        type: String,
+        trim: true,
+        required: [true, 'Please add a title'],
+        index: true
+    },
+    shortDesc: {
+        type: String,
+        trim: true,
+        required: [true, 'Please add a short description'],
+        maxLength: [100, 'Short description may only have 100 characters or fewer']
+    },
+    longDesc: {
+        type: String,
+        trim: true,
+        required: [true, 'Please add a long description']
+    },
+    markedAsComplete: {
+        type: Boolean,
+        default: false
+    },
+    dateLastUpdated: {
+        type: Date,
+        default: Date.now,
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now,
+    },
+    viewCount: {
+        type: Number,
+        min: 0,
+        default: 0
+    },
+    registrationCount: {
+        type: Number,
+        min: 0,
+        default: 0,
+    },
+    reviewCount: {
+        type: Number,
+        min: 0,
+        default: 0,
+    },
+    averageRatingStars: {
+        type: Number,
+        min: 0,
+        max: 5,
+    },
+    reviews: [mongoose.Types.ObjectId],
+    thumbnailSmall: String,
+    thumbnailLarge: String,
+    basePrice: {
+        type: Number,
+        required: [true, 'Please add a price'],
+        min: 0
+    },
+    discountedPrice: {
+        type: Number,
+        min: 0
+    }
+})
+
+
+const Course = mongoose.model('Course', courseSchema)
+
+export { ICourse, Course }
