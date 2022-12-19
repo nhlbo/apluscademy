b81cd15b779e98bb81dc9465b617548dd4794d18
Chánh Quang <47215667+MeanSoybean@users.noreply.github.com>
2022-12-19 17:17:43 +0700

    feat: db modeling course, category, review
    added schema for courses, course categories, and reviews
    see Trello task db modeling iteration 2 for reqs on syllabus content

---
+++ src/models/review.ts
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose'
+import { ICourse } from './course'
+import { IUser } from './user'
+
+interface IReview {
+    course: ICourse
+    author: IUser
+    feedback: string
+    ratingStars: number
+}
+
+const reviewSchema = new mongoose.Schema({
+    course: {
+        type: mongoose.Types.ObjectId,
+        required: [true, 'Review must be done on a course'],
+    },
+    author: {
+        type: mongoose.Types.ObjectId,
+        required: [true, 'Review must have an author']
+    },
+    ratingStars: {
+        type: Number,
+        min: 0,
+        max: 5,
+        required: [true, 'Please add a star rating']
+    },
+    feedback: {
+        type: String
+    }
+})
+
+
+const Review = mongoose.model('Course', reviewSchema)
+export { IReview, Review }
