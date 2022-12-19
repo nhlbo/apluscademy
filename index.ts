105eb07c481a092c102c0d465dde72a64a2d4dab
Long Nguyen <nhlbo@users.noreply.github.com>
2022-12-19 10:45:47 +0700

    feat: add login with facebook

--- index.ts
+++ index.ts
@@ -2,24 +2,27 @@ import express from 'express'
 import session from 'express-session'
 import mongoose from 'mongoose'
 import passport from 'passport'
+import flash from 'express-flash'
 import { authRoute, homeRoute } from './src/routes'
 
 const app = express()
 const PORT = process.env.PORT || 3000
 const MONGO_DB_URL = process.env.MONGO_DB_URL || ''
+const SESSION_SECRET = process.env.SESSION_SECRET || ''
 
 mongoose.connect(MONGO_DB_URL).then(() => console.log('Connected to the database.'))
 let SQLiteStore = require('connect-sqlite3')(session)
 
 app.use(
   session({
-    secret: 'keyboard cat',
+    secret: SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     store: new SQLiteStore()
   })
 )
 app.use(passport.authenticate('session'))
+app.use(flash())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import passport from 'passport'
 
 import * as authController from '../controllers/auth'
 
@@ -7,6 +8,13 @@ const router = express.Router()
 router.route('/register').post(authController.postRegister)
 router.route('/login').get(authController.getLogin)
 router.route('/login/password').post(authController.postLoginPassword)
+router.route('/oauth2/redirect/facebook').get(passport.authenticate('facebook'))
+router
+  .route('/auth/facebook/callback')
+  .get(passport.authenticate('facebook', { failureRedirect: '/login' }), (_, res) => {
+    res.redirect('/')
+  })
+
 router.route('/logout').post(authController.postLogout)
 
 export { router as authRoute }
