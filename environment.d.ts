807e2e973977e6d46eafb9e267fc4820de1660a1
Long Nguyen <nhlbo@users.noreply.github.com>
2022-12-20 15:18:46 +0700

    feat: add login with google

--- environment.d.ts
+++ environment.d.ts
@@ -8,6 +8,8 @@ declare global {
       SESSION_SECRET: string
       FACEBOOK_ID: string
       FACEBOOK_SECRET: string
+      GOOGLE_CLIENT_ID: string
+      GOOGLE_CLIENT_SECRET: string
     }
   }
 }
