940e68ee2d1d08a0dba435f2193cac0f3ed36d04
ldnhn <62146587+ldnhn@users.noreply.github.com>
2023-01-02 15:19:42 +0700

    feat: add add, delete, edit category

--- src/routes/category.ts
+++ src/routes/category.ts
@@ -1,5 +1,13 @@
 import express from 'express'
-import { getAddCategory, postAddCategory, getCategoryList, getCourseList, getViewCourse } from '../controllers/category'
+import {
+  getAddCategory,
+  postAddCategory,
+  getCategoryList,
+  getCourseList,
+  getViewCourse,
+  postDeleteCategory,
+  postEditCategoryName
+} from '../controllers/category'
 
 const router = express.Router()
 
@@ -7,7 +15,9 @@ router.route('/add').get(getAddCategory)
 router.route('/add').post(postAddCategory)
 
 router.route('/view').get(getCategoryList)
-router.route('/view/:dynamic_cate_id').get(getCourseList)
-router.route('/view/course/:dynamic_course_id').get(getViewCourse)
+router.route('/view/:cate_id').get(getCourseList)
+router.route('/view/course/:course_id').get(getViewCourse)
+router.route('/delete/:course_id').get(postDeleteCategory)
+router.route('/edit/:cate_id').post(postEditCategoryName)
 
 export { router as CategoryRoute }
