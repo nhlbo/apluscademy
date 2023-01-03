f1a88cbe5a3558426608c66c4d6a8f3d32766322
Long Nguyen <nhlbo@users.noreply.github.com>
2023-01-03 14:52:03 +0700

    feat: upload image to file system

--- environment.d.ts
+++ environment.d.ts
@@ -13,6 +13,10 @@ declare global {
       APLUSCADEMY_USERNAME: string
       APLUSCADEMY_PASSWORD: string
       CALLBACK_DOMAIN: string
+      AWS_ACCESS_KEY_ID: string
+      AWS_SECRET_ACCESS_KEY: string
+      AWS_DOMAIN_NAME: string
+      AWS_BUCKET: string
     }
   }
 }
---
+++ src/utils/amazonS3.ts
@@ -0,0 +1,32 @@
+import { S3Client } from '@aws-sdk/client-s3'
+import multer from 'multer'
+import multerS3 from 'multer-s3'
+
+const AWS_DOMAIN_NAME = process.env.AWS_DOMAIN_NAME!
+const AWS_BUCKET = process.env.AWS_BUCKET!
+const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID!
+const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY!
+
+const s3 = new S3Client({
+  endpoint: `https://${AWS_DOMAIN_NAME}`,
+  region: 'ap-southeast-1',
+  credentials: {
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY
+  }
+})
+
+const upload = (fileName: string) =>
+  multer({
+    storage: multerS3({
+      s3: s3,
+      bucket: AWS_BUCKET,
+      acl: 'public-read',
+      key: function (_request, _file, cb) {
+        cb(null, fileName)
+      },
+      contentType: multerS3.AUTO_CONTENT_TYPE
+    })
+  }).array('upload', 1)
+
+export { upload }
