404bbf04610f7420aa03ebfc8da59305460031e6
ldnhn <62146587+ldnhn@users.noreply.github.com>
2023-01-04 20:43:47 +0700

    feat: add view a student

--- src/routes/student.ts
+++ src/routes/student.ts
@@ -1,13 +1,10 @@
 import express from 'express'
-import { getStudentList, postDeleteStudent } from '../controllers/student'
+import { getStudent, getStudentList, postDeleteStudent } from '../controllers/student'
 
 const router = express.Router()
 
-// router.route('/add').get(getAddLecturer)
-// router.route('/add').post(postAddLecturer)
-
 router.route('/').get(getStudentList)
-//router.route('/:id').get(getLecturer)
+router.route('/:id').get(getStudent)
 
 router.route('/delete/:id').post(postDeleteStudent)
 
