e0458a1c81b597198b82ae6edf3f7d165125e741
ldnhn <62146587+ldnhn@users.noreply.github.com>
2023-01-07 16:22:34 +0700

    fix: styling lecturer & student

--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -7,10 +7,12 @@ const router = express.Router()
 router.route('/').get(profileController.getProfile)
 router.route('/edit/avatar').post(upload.single('file'), profileController.updateAvatar)
 
-router.route('/edit/name').get(profileController.getChangeName)
 router.route('/edit/name').post(profileController.postChangeName)
 
-router.route('/edit/password').get(profileController.getChangePassword)
 router.route('/edit/password').post(profileController.postChangePassword)
+
+router.route('/edit/email').post(profileController.postChangeEmail)
+
+router.route('/verify').post(profileController.postVerifyOtp)
 
 export { router as profileRoute }
