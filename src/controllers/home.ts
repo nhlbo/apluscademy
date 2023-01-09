8e3365fd5b751bd07d6eb76f287e614ab8b4c807
Chánh Quang <47215667+MeanSoybean@users.noreply.github.com>
2023-01-10 02:38:44 +0700

    feat homepage

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -1,12 +1,17 @@
 import { asyncHandler } from '../middlewares/async'
 import { getCategories } from '../utils/utils'
+import { Course } from '../models/course'
 
 const getHome = asyncHandler(async (req, res) => {
   const categoriesResult = await getCategories()
+  const mostViewedCourses = await Course.find({}).limit(10).sort({ viewCount: -1 })
+  const newestCourses = await Course.find({}).limit(10).sort({ dateCreated: -1 })
   res.render('pages/index', {
     isAuthenticated: req.isAuthenticated(),
     avatar: req.cookies.avatar,
-    categories: categoriesResult
+    categories: categoriesResult,
+    mostViewedCourses: mostViewedCourses,
+    newestCourses: newestCourses
   })
 })
 
